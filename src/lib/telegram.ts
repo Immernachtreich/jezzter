774d89e6c5d802e0da93ec5324e83ac7083d3edd
Ebrahim Tohfafarosh <ebrahim@blackcurrantlabs.com>
2024-01-21 14:18:09 +0530

    fix: fixed env config

--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -1,3 +1,3 @@
 import TelegramBot from 'node-telegram-bot-api';
 
-export const bot = new TelegramBot(process.env.BOT_TOKEN!, { polling: true });
+export const jezzterBot = new TelegramBot(process.env.BOT_TOKEN!);
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,17 +1,28 @@
 import 'reflect-metadata';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config({ path: path.join(__dirname, '..', '..', '.env') });
+
 import express, { Request, Response } from 'express';
 import next from 'next';
 import { parse } from 'url';
+
+import { jezzterBot } from '../lib/telegram';
 import { sequelize } from '../lib/sequelize';
-import { bot } from '../lib/telegram';
 
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handle = app.getRequestHandler();
 
+/**
+ * Main entry point of the application
+ * Server starts here with bot and datbase connection
+ * @returns {Promise<void>}
+ */
 (async function initialize() {
   try {
     await app.prepare();
 
+    // Direct all traffic from express to next server
     const server = express();
     server.all('*', (request: Request, response: Response) => {
       const parsedUrl = parse(request.url, true);
@@ -19,8 +30,12 @@ const handle = app.getRequestHandler();
       handle(request, response, parsedUrl);
     });
 
+    // Authenticate SQL server
     await sequelize.authenticate();
     console.log('Connection established to database');
+
+    // Start Telegram bot
+    await jezzterBot.startPolling();
 
     server.listen(process.env.PORT ?? 3000, () =>
       console.log(`Server listening on port: ${process.env.PORT ?? 3000}`)
