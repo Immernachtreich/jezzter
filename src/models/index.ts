15382ae28a40b59ade533a326c70b1809dba0056
Ebrahim Tohfafarosh <ebrahim@blackcurrantlabs.com>
2024-01-23 16:44:02 +0530

    upt: added models to index file

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,6 @@
 import { sequelize } from '@/lib/sequelize';
 import { User } from './user.model';
+import File from './file.model';
+import FileChunk from './file-chunks.model';
 
-export { User, sequelize };
+export { User, File, FileChunk, sequelize };
