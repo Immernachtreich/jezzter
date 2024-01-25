0149dd042c495377014b511b5e275f9ea21e78c2
Ebrahim Tohfafarosh <ebrahim@blackcurrantlabs.com>
2024-01-25 15:19:35 +0530

    added vercel config

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,10 @@
+import { Jwt } from 'jsonwebtoken';
+
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production';
     PORT: number;
     BOT_TOKEN: string;
+    JWT_SECRET: string;
   }
 }
